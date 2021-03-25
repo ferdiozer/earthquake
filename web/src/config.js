f4dd691f18effe6e5b2fa0a0b7a480fd5f3c600d
Ferdi Özer <info@ferdiozer.com>
2021-03-25 18:42:40 +0300

    update api url config

--- web/src/config.js
+++ web/src/config.js
@@ -1,5 +1,7 @@
 
 
-export const API_URL = "http://localhost:3080/api";
+const url = process.env.NODE_ENV !== "development" ? "https://deprem.piyanos.com" : "http://localhost:3070"
+
+export const API_URL = `${url}/api`;
 
 export const GOOGLE_MAP_API_KEY = "AIzaSyB9TGPjEVJOnkrsGZIGFywFmub1nENYHg0";
